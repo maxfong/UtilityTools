e7f5522124c187143edd31288d9e5e8fdb5c9fc7
fal3750 <fal3750@17u.cn>
2014-09-13 17:59:33 +0800

    add EntityModel _test

---
+++ ConvertTools/TCTEntityModel/TCTEntityFilesResponseModel.h
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Auto Created by maxfong's UtilityTools on ___DATE___.
+//  Operation Person Name is ___NAME___
+//___COPYRIGHT___
+//
+
+___IMPORTHEADER_cocoaTouchSubclass___
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : ___VARIABLE_cocoaTouchSubclass___
+
+___PropertyName___
+
+@end
---
+++ ConvertTools/TCTEntityModel/TCTEntityFilesResponseModel.m
@@ -0,0 +1,14 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Auto Created by maxfong's UtilityTools on ___DATE___.
+//  Operation Person Name is ___NAME___
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@end
